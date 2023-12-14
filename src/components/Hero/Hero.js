e4bd0a4f0ee2d0ccbc21aaf949b232018b6906bc
Piotr Kotulski <piotr.kotulski1986@gmail.com>
2023-12-15 00:03:51 +0100

    fixed css modules

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,11 @@
-
-import './Hero.module.scss';
+import styles from './Hero.module.scss';
 
 
 const Hero = () => {
     return (
-        <div className="hero">
-            <h1 className="title">My first React App</h1>
-            <p className="subtitle">A simple to-do app, with lists, columns and card</p>
+        <div className={styles.hero}>
+            <h1 className={styles.title}>My first React App</h1>
+            <p className={styles.subtitle}>A simple to-do app, with lists, columns and card</p>
         </div>
     );
 };
