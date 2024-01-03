9d65e0d9f133e6a413020250b7b7ed51dca228aa
piotr kotulski <piotr.kotulski@exorigo-upos.pl>
2024-01-03 23:50:23 +0100

    redux implemented

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,12 @@
 import styles from "./Column.module.scss";
 import Card from "../Card/Card";
 import CardForm from "../CardForm/CardForm";
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
 
     return (
         <article className={styles.column}>
@@ -10,9 +14,9 @@ const Column = props => {
                 <span className={styles.icon + ' fa fa-' + props.icon}/>
                 {props.title}</h2>
             <ul className={styles.cards}>
-                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+                {cards.map(card => <Card key={card.id} title={card.title} />)}
             </ul>
-            <CardForm columnId={props.id} addCard={props.addCard} />
+            <CardForm columnId={props.id}  />
         </article>
     )
 }
