3290587c05c317414cd0eb7b41d3a67d4ed498b6
piotr kotulski <piotr.kotulski@exorigo-upos.pl>
2024-01-07 22:58:10 +0100

    added remove card option

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -9,6 +9,7 @@ export const getFilteredCards = ({cards, searchString}, columnId) => cards
 const createActionName = actionName => `app/lists/${actionName}`;
 const ADD_CARD = createActionName('ADD_COLUMN');
 const TOGGLE_CARD_FAVORITE = createActionName('TOGGLE_CARD_FAVORITE');
+const REMOVE_CARD = createActionName('REMOVE_CARD');
 
 // action creators
 export const addCard = ({title, columnId}) => ({type: ADD_CARD, payload: {title, columnId}});
@@ -18,6 +19,10 @@ export const toggleCardFavorite = (cardId) => {
         payload: cardId,
     };
 };
+export const removeCard = (cardId) => ({
+    type: REMOVE_CARD,
+    payload: cardId,
+});
 
 
 const cardsReducer = (statePart = [], action) => {
@@ -26,6 +31,8 @@ const cardsReducer = (statePart = [], action) => {
             return [...statePart, { ...action.payload, id: shortid() }];
         case TOGGLE_CARD_FAVORITE:
             return statePart.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card);
+        case REMOVE_CARD:
+            return statePart.filter(card => card.id !== action.payload);
         default:
             return statePart;
     }
